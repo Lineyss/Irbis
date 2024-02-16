286d26c1987e62b309c65de2b1ee6e2f9db281ab
Lineyss <sascha.ionenko@yandex.ru>
2024-02-16 13:20:38 +0300

    Update-prodaction

--- Irbis/Irbis/settings.py
+++ Irbis/Irbis/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "main.middleware.CustomDataMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "Irbis.urls"
