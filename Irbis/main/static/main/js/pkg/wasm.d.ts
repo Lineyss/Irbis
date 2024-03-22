4bd63272f1f7c02bce7238bad2252df3750ca9e7
Lineyss <sascha.ionenko@yandex.ru>
2024-03-22 12:59:53 +0300

    pkg

---
+++ Irbis/main/static/main/js/pkg/wasm.d.ts
@@ -0,0 +1,35 @@
+declare namespace wasm_bindgen {
+	/* tslint:disable */
+	/* eslint-disable */
+	/**
+	*/
+	export function init_log(): void;
+	/**
+	* @param {string} data
+	* @returns {Float32Array}
+	*/
+	export function step_to_triangle_buf(data: string): Float32Array;
+	
+}
+
+declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+declare interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly init_log: () => void;
+  readonly step_to_triangle_buf: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
